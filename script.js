674b72432bdf153f1271703b9c07156d68f11830
CrpticHate92 <145369850+CrpticHate92@users.noreply.github.com>
2023-11-20 10:18:17 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById("btn1").addEventListener("click", function1);
+document.getElementById("btn2").addEventListener("click", function2);
+document.getElementById("btn3").addEventListener("click", function3);
+document.getElementById("btn4").addEventListener("click", function4);
+document.getElementById("btn5").addEventListener("click", function5);
+
+function function1() {
+  alert("comes great responsability");
+}
+function function2() {
+  alert("and mouse");
+}
+function function3() {
+  alert("thin");
+}
+function function4() {
+  alert("as nails");
+}
+function function5() {
+  alert("blue moon");
+}
